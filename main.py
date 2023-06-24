376276fbb0229c0b2e9d17ea8902e17fd0abd88a
Yash Gupta <108118711+yashpapa6969@users.noreply.github.com>
2023-06-24 14:52:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from flask import Flask, jsonify
-import os
+import json
+from flask import Flask, request, jsonify
+import io
+import requests
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, request, jsonify
 
+model = tf.keras.models.load_model('imageclassifier.h5')
 app = Flask(__name__)
 
+@app.route('/predict', methods=['POST'])
+def hello():
+    # Get the image URL from the request
+    image_url = request.json['image_url']
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    # Download the image from the URL
+    response = requests.get(image_url)
+    
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to download image.'}), 400
 
+    # Decode and preprocess the image
+    image = Image.open(io.BytesIO(response.content))
+    image = image.resize((224, 224))
+    image = np.array(image)
+    image = np.expand_dims(image, axis=0)
 
+    # Make predictions using the loaded TensorFlow model
+    predictions = model.predict(image)
+
+    # Process the predictions as needed
+    # ...
+    predictions1 = np.round(predictions)
+    # Return the predictions as a JSON response
+    return jsonify({'predictions': predictions1.tolist()})
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
